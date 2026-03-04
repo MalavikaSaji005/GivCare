3868293de91abc9fc4dbc2e5974af39f13b80734
Jumana jadheeda pj <149096011+jumanajadheeda@users.noreply.github.com>
2026-03-04 11:14:50 +0530

    Implemented Donate to Need page with filters, modal, progress bar, and Firebase integration

--- src/App.js
+++ src/App.js
@@ -4,16 +4,37 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import InstitutionDashboard from "./pages/InstitutionDashboard";
 import BrowseNeeds from "./pages/BrowseNeeds";
-
+import DonationHistory from "./pages/DonationHistory";
+import { useState } from "react";
 function App() {
+  const [donations, setDonations] = useState([]);
   return (
+
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/institution" element={<InstitutionDashboard />} />
-        <Route path="/browse" element={<BrowseNeeds />} />
+        <Route
+          path="/browse"
+          element={
+            <BrowseNeeds
+              donations={donations}
+              setDonations={setDonations}
+            />
+          }
+        />
+
+        <Route
+          path="/donation-history"
+          element={
+            <DonationHistory
+              donations={donations}
+              setDonations={setDonations}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
